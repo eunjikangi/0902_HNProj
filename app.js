7cf29329b605d65c9bd0ff69cf0488c0325b6b40
eunji kang <miki2323@naver.com>
2024-09-03 07:49:57 +0900

    for문으로 변경

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const ajax = new XMLHttpRequest();
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 
-ajax.open('GET', 'https://api.hnpwa.com/v0/news/1.json', false);
+ajax.open('GET', NEWS_URL, false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response)
+const ul = document.createElement('ul')
 
-document.getElementById('root').innerHTML =
-`<ul> 
-  <li>${newsFeed[0].title}</li>
-  <li>${newsFeed[1].title}</li>
-  <li>${newsFeed[2].title}</li>
-</ul>`;
+for (let i = 0; i< 10; i++) {
+  const li = document.createElement('li')
+  li.innerHTML = newsFeed[i].title
+
+  ul.appendChild(li)
+}
+
+document.getElementById('root').appendChild(ul)
