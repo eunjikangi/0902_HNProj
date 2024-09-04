0a48256be8eea47fd5e446327cd853db4e85e080
eunji kang <miki2323@naver.com>
2024-09-05 07:37:46 +0900

    Refac

--- app.js
+++ app.js
@@ -5,39 +5,39 @@ const content = document.createElement('div')
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+  return JSON.parse(ajax.response)
+}
 
-const newsFeed = JSON.parse(ajax.response)
+const newsFeed = getData(NEWS_URL)
 const ul = document.createElement('ul')
 
 window.addEventListener('hashchange', function() {
-  // console.log(this.location.hash); // 현재 location의 hash값을 console에 출력.
+  // console.log(this.location.hash); 
+  // 현재 location의 hash값을 console에 출력.
   const id = location.hash.substr(1);
-
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false)
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace('@id', id))
   const title = this.document.createElement('h1')
   
   title.innerHTML = newsContent.title;
   content.appendChild(title)
-  console.log(newsContent)
 })
 
+for (let i = 0; i< 10; i++) 
+{
+  const div = document.createElement('div')
+  div.innerHTML = `
+  <li>
+    <a href="#${newsFeed[i].id}">
+      ${newsFeed[i].title} (${newsFeed[i].comments_count})
+    </a>
+  </li>
+  `
 
-for (let i = 0; i< 10; i++) {
-  const li = document.createElement('li')
-  const a = document.createElement('a')
-
-  // <a href=""></a>
-  a.href = `#${newsFeed[i].id}`
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`
-
-  li.appendChild(a)
-  ul.appendChild(li)
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul)
