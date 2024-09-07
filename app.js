4c134be04b1fd6bcfb3f958db5d02e949a398348
eunji kang <miki2323@naver.com>
2024-09-07 11:13:04 +0900

    EndPage 처리 추가

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const content = document.createElement('div')
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
 
+const store = {
+  currentPage: 1,
+};
+
 function getData(url) {
   ajax.open('GET', url, false);
   ajax.send();
@@ -17,11 +21,12 @@ function newsFeed() {
   const newsList = []
 
   newsList.push('<ul>')
-  for (let i = 0; i< 10; i++) 
+
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) 
   {
     newsList.push(`
     <li>
-      <a href="#${newsFeedData[i].id}">
+      <a href="#/show/${newsFeedData[i].id}">
         ${newsFeedData[i].title} (${newsFeedData[i].comments_count})
       </a> 
     </li>
@@ -29,18 +34,32 @@ function newsFeed() {
   }
   newsList.push('</ul>')
 
-  // join : 배열 안 문자열들을 하나의 문자열로 합쳐서 return해줌.
+  let bEndPage = false
+  if (newsFeedData[store.currentPage * 10] == undefined)
+    {
+      bEndPage = true
+    }
+
+  newsList.push(`
+    <div>
+      <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+      <a href="#/page/${bEndPage ? store.currentPage : store.currentPage + 1}">다음 페이지</a>
+    </div>
+    `);
+
   container.innerHTML = newsList.join('')
 }
 
 function newsDetail() {
-  const id = location.hash.substr(1);
+  console.log('newsDetail')
+
+  const id = location.hash.substr(7);
   const newsContent = getData(CONTENT_URL.replace('@id', id))
   
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
      </div>
   `
 }
@@ -49,6 +68,11 @@ function router() {
   const routePath = location.hash;
 
   if (routePath == '') {
+    newsFeed()
+    console.log('Init')
+  }
+  else if (routePath.indexOf('#/page/') >= 0) {
+    store.currentPage = Number(routePath.substr(7));
     newsFeed()
   }
   else {
@@ -59,3 +83,4 @@ function router() {
 window.addEventListener('hashchange', router)
 
 newsFeed();
+console.log('초기화')
